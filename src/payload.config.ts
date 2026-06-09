e544858b965edeb8c21a0942ef2053a9ee87be85
Christin6 <94294443+Christin6@users.noreply.github.com>
2026-06-09 19:55:33 +0700

    remove some useless files

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,9 +1,9 @@
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
